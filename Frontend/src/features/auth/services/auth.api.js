3bab129f169e6658cf570eb10be468e77ec7a09b
VinayChoudhary12 <vinaychoudhary56803@gmail.com>
2026-08-27 00:01:37 +0530

    update Cors

--- Frontend/src/features/auth/services/auth.api.js
+++ Frontend/src/features/auth/services/auth.api.js
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 const api = axios.create({
-    baseURL: "https://backend1-a4h7.onrender.com",
+    baseURL: import.meta.env.VITE_BASE_URL,
     withCredentials: true
 })
 
