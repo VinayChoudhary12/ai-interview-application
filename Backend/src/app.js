84ef6444e058a3ca2e0a8c3ac4cf4354d4247b04
VinayChoudhary12 <vinaychoudhary56803@gmail.com>
2026-08-26 23:51:35 +0530

    update Cors

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -7,7 +7,7 @@ const app = express()
 app.use(express.json())
 app.use(cookieParser())
 app.use(cors({
-    origin: "http://localhost:5173",
+    origin: "https://frontend3-0jfb.onrender.com",
     credentials: true
 }))
 
